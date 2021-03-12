b8dfd1135b10fa287b5acb1312c651c05afbfcc8
bruno <bruno.tanaka@e-deploy.com.br>
2021-03-12 16:23:32 -0400

    do not show layout to login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { Fragment, ReactElement } from 'react'
 import { AppProps } from 'next/app'
 import '../styles/global.css'
 import { ToastContainer } from 'react-toastify'
@@ -6,12 +6,16 @@ import { AuthProvider } from '../contexts/AuthContext'
 import 'react-toastify/dist/ReactToastify.css'
 import Layout from '../components/Layout'
 
-function MyApp({ Component, pageProps }: AppProps): ReactElement {
+function MyApp({ Component, pageProps, router }: AppProps): ReactElement {
+  const canShowLayout = !router.pathname.startsWith('/login')
+
+  const WrapperLayout = canShowLayout ? Layout : Fragment
+
   return (
     <AuthProvider>
-      <Layout>
+      <WrapperLayout>
         <Component {...pageProps} />
-      </Layout>
+      </WrapperLayout>
       <ToastContainer pauseOnFocusLoss={false} pauseOnHover={false} />
     </AuthProvider>
   )
